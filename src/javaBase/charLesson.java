81e4613d8bf959b4d146d859410fbbb35271ad55
liuhting <huiting.liu@qq.com>
2019-07-05 15:53:07 +0800

    Character类

---
+++ src/javaBase/charLesson.java
@@ -0,0 +1,15 @@
+package javaBase;
+//Character类
+public class charLesson {
+
+    public  static  void main(String[] args){
+        Character ch = new Character('z');
+        Character ch1 = 'f';
+        System.out.println(Character.toUpperCase('c'));
+        System.out.println(Character.toLowerCase('Y'));
+        System.out.println("是一个字母吗："+Character.isLetter('Y'));
+        System.out.println("是一个数字吗："+Character.isDigit('Y'));
+        System.out.println(Character.isDigit('5'));
+        System.out.println(Character.toString('Y'));
+    }
+}
