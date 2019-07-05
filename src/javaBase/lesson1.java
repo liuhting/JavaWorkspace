ba158862013786381bc45609745ec412b8027ab5
liuhting <huiting.liu@qq.com>
2019-07-05 14:07:13 +0800

    有添加了点内容

--- src/javaBase/lesson1.java
+++ src/javaBase/lesson1.java
@@ -3,5 +3,6 @@ package javaBase;
 public class lesson1 {
     public  static void main(String[] args){
         System.out.println("Hello,world");
+        System.out.println("Hello,susan");
     }
 }
